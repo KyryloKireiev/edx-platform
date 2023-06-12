fccd558415d1ab53a9fbeca130eee3c79416b3b4
KyryloKireiev <kirillkireev888@gmail.com>
2023-06-12 18:34:00 +0300

    remove unused imports and extra comments

--- lms/djangoapps/onboarding/views.py
+++ lms/djangoapps/onboarding/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from common.djangoapps.edxmako.shortcuts import render_to_response
-from lms.djangoapps.learner_dashboard.programs import (
-    ProgramDetailsFragmentView,
-    ProgramDiscussionLTI,
-    ProgramsFragmentView, ProgramLiveLTI)
+from lms.djangoapps.learner_dashboard.programs import ProgramsFragmentView
 from openedx.core.djangoapps.programs.models import ProgramsApiConfig
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
@@ -13,6 +8,7 @@ from django.views.decorators.http import require_GET
 # test response
 # def get_onboarding_page(request):
 #     return HttpResponse("Onboarding page")
+
 @login_required
 @require_GET
 def get_onboarding_page(request):
@@ -26,6 +22,4 @@ def get_onboarding_page(request):
         'show_program_listing': programs_config.enabled,
         'uses_bootstrap': True,
     }
-    # return render_to_response('learner_dashboard/programs.html', context)
     return render_to_response('onboarding.html', context)
-    # return render(request, 'onboarding_page.html', context)
