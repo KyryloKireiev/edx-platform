c29f17debf2f12298e7e3b4bfca0aead3043b03c
KyryloKireiev <kirillkireev888@gmail.com>
2023-06-13 11:24:12 +0300

    add translations to onboarding page

--- lms/djangoapps/onboarding/views.py
+++ lms/djangoapps/onboarding/views.py
@@ -1,5 +1,4 @@
 from common.djangoapps.edxmako.shortcuts import render_to_response
-from lms.djangoapps.learner_dashboard.programs import ProgramsFragmentView
 from openedx.core.djangoapps.programs.models import ProgramsApiConfig
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
@@ -13,10 +12,8 @@ from django.views.decorators.http import require_GET
 @require_GET
 def get_onboarding_page(request):
     programs_config = ProgramsApiConfig.current()
-    programs_fragment = ProgramsFragmentView().render_to_fragment(request, programs_config=programs_config)
     context = {
         'disable_courseware_js': True,
-        'programs_fragment': programs_fragment,
         'nav_hidden': True,
         'show_dashboard_tabs': True,
         'show_program_listing': programs_config.enabled,
